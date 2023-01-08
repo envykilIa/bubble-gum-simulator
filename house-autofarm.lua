c8532cbe603faa9da84b0a27282b505606798209
envykilIa <113392869+envykilIa@users.noreply.github.com>
2023-01-08 15:28:01 -0800

    Create house-autofarm.lua

---
+++ house-autofarm.lua
@@ -0,0 +1,66 @@
+-- house 1
+local TweenService = game:GetService("TweenService")
+wait(2)
+local Tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2), {CFrame = CFrame.new(-30.6183662, 254.816681, 1559.26709, 0.0998725668, -8.95688745e-19, -0.995000243, -1.75748435e-21, 1, -9.00365831e-19, 0.995000243, 9.16705466e-20, 0.0998725668)})
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = true
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Velocity = Vector3.new()
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").RotVelocity = Vector3.new()
+wait(2.1)
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = false
+Tween:Play()
+wait(1)
+-- house 2
+local TweenService = game:GetService("TweenService")
+wait(2)
+local Tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2), {CFrame = CFrame.new(18.038559, 279.778625, 1610.93005, -0.924106181, -1.76583997e-10, -0.382135779, -1.75855632e-11, 1, -4.19570961e-10, 0.382135779, -3.81008058e-10, -0.924106181)})
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = true
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Velocity = Vector3.new()
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").RotVelocity = Vector3.new()
+wait(2.1)
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = false
+Tween:Play()
+wait(2)
+-- house 3
+local TweenService = game:GetService("TweenService")
+wait(2)
+local Tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2), {CFrame = CFrame.new(76.8590698, 272.851898, 1583.76074, -0.294149816, -1.30752241e-11, -0.955759346, -4.66378672e-13, 1, -1.35369216e-11, 0.955759346, -3.53613709e-12, -0.294149816)})
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = true
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Velocity = Vector3.new()
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").RotVelocity = Vector3.new()
+                
+Tween:Play()
+wait(2.1)
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = false
+-- house 4
+local TweenService = game:GetService("TweenService")
+wait(2)
+local Tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2), {CFrame = CFrame.new(85.2692184, 266.585907, 1534.87866, 0.160496205, -1.17969717e-10, -0.987036467, -4.29998467e-11, 1, -1.26511066e-10, 0.987036467, 6.27469604e-11, 0.160496205)})
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = true
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Velocity = Vector3.new()
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").RotVelocity = Vector3.new()
+                
+Tween:Play()
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = false
+wait(2.2)
+-- house 5
+local TweenService = game:GetService("TweenService")
+wait(2)
+local Tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2), {CFrame = CFrame.new(27.4988842, 260.663208, 1515.43628, -0.036470864, -3.94019821e-08, -0.999334693, -3.80400209e-08, 1, -3.80399392e-08, 0.999334693, 3.66273625e-08, -0.036470864)})
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = true
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Velocity = Vector3.new()
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").RotVelocity = Vector3.new()
+                
+Tween:Play()
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = false
+wait(2.2)
+-- house 6
+local TweenService = game:GetService("TweenService")
+wait(2)
+local Tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2), {CFrame = CFrame.new(38.7753448, 271.166138, 1487.0332, 0.663384914, 5.36997646e-08, -0.74827832, -3.80400387e-08, 1, 3.8040092e-08, 0.74827832, 3.22931459e-09, 0.663384914)})
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = true
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Velocity = Vector3.new()
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").RotVelocity = Vector3.new()
+                
+Tween:Play()
+game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = false
+wait(2.2)
